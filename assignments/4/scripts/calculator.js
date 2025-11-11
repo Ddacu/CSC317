eb5aa4b8ba43b27fb046637ffce3dbf243451623
Ddacu <ddacumos1@sfsu.edu>
2025-11-11 15:12:58 -0800

    Added in Comments

--- assignments/4/scripts/calculator.js
+++ assignments/4/scripts/calculator.js
@@ -4,41 +4,54 @@ let currentValue = ")";
 let plusMinus = "+";
 
 function addButtonValue(input) {
+
+    // If input equals AC this will reset it to 0
    if(input == "AC"){
         output.value = "";
         values.value = "";
 
         return;
-    } else if (input == "()"){
+    }
+    
+    // Based off the variables above we will assume we start with ), so that 
+    // on first run and pressing the button, it will start with ( and the
+    // currentValue tracks it, so that each time it can swap between them.
+    if (input == "()"){
 
         if(currentValue == ")"){
             currentValue = "(";
-            output.value += "(";
+            input = "(";
 
         } else if (currentValue == "("){
             currentValue = ")";
-            output.value += ")";
+            input = ")";
         }
-
-        return;
     }
+
 
     if(input == "+/-"){
 
-    // If it's postive value already it will become a negative
-        if(!(output.value.startsWith("-"))){
+    // If it's postive value already it will become a negative and if it's
+    // already negative at the start it will remove it from the output.value
+    // array
+        if(output.value[0] != "-"){
             output.value = "-" + output.value;
 
-        } else if (output.value.startsWith("-")){
+        } else if (output.value[0] == "-"){
             output.value = output.value.slice(1);
         }
+
         return;
     }
 
+    // Runs the calculations
     if(input == "="){
        output.value = eval(output.value);
        return;
     }
 
+    // Adds the input, so that it appears on the input screen
     output.value += input;
+
+    return;
 }
