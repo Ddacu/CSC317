3ba4313abca9afdfb10ea38b65f76769e4de169d
Daniel Jose Dacumos <ddacumos1@sfsu.edu>
2025-11-12 08:45:53 -0800

    Adding Calculator to HTML

--- assignments/4/scripts/calculator.js
+++ assignments/4/scripts/calculator.js
@@ -1,39 +1,51 @@
 const output = document.getElementById("output");
 let values;
-let currentValue = ")";
+let paraCheck = ")";
 let plusMinus = "+";
 
-function addButtonValue(input) {
+function addValue(input) {
 
-    // If input equals AC this will reset it to 0
-   if(input == "AC"){
+    // If input equals AC this will reset it to 0 and takes
+    // priority over the error message
+    if(input == "AC"){
         output.value = "";
-        values.value = "";
-
         return;
+
+    // This prevents the user from pressing the buttons
+    // during this error screen
+    } else if(output.value == "Invalid (Press AC)"){
+        input = "";
     }
     
     // Based off the variables above we will assume we start with ), so that 
     // on first run and pressing the button, it will start with ( and the
-    // currentValue tracks it, so that each time it can swap between them.
+    // paraCheck tracks it, so that each time it can swap between them.
+    // However, it also check if the previous ( is the same one
     if (input == "()"){
-
-        if(currentValue == ")"){
-            currentValue = "(";
+        
+        if(paraCheck == ")"){
+            paraCheck = "(";
             input = "(";
 
-        } else if (currentValue == "("){
-            currentValue = ")";
+        } else if (paraCheck == "("){
+            paraCheck = ")";
             input = ")";
         }
+
+        if(output.value[output.value.length - 1] == "("){
+            input = "(";
+
+        } else if(output.value[output.value.length - 1] == ")"){
+            input = ")"; 
+        }
+
     }
-
-
-    if(input == "+/-"){
 
     // If it's postive value already it will become a negative and if it's
     // already negative at the start it will remove it from the output.value
     // array
+    if(input == "+/-"){
+
         if(output.value[0] != "-"){
             output.value = "-" + output.value;
 
@@ -44,10 +56,25 @@ function addButtonValue(input) {
         return;
     }
 
-    // Runs the calculations
+    // Changes them to symbols that can be used in the calculations
+    if(input == "÷"){
+        input = "/";
+    } else if (input == "×"){
+        input = "*";
+        
+    }
+    
+    // Uses the eval function to calculate, but also tries to catch
+    // if there's an error with the calculation to tell the user
+    // it won't work    
     if(input == "="){
-       output.value = eval(output.value);
-       return;
+        try {
+            output.value = eval(output.value);
+        } catch (error) {
+            output.value = "Invalid (Press AC)"; 
+        }
+        
+        return;
     }
 
     // Adds the input, so that it appears on the input screen
